2729e0cd5361eb188b972a72dcd28f773b70d673
jeong0982 <jeong0982@kaist.ac.kr>
2023-08-07 16:12:11 +0900

    Add vm code [WIP]

--- src/vm/mod.rs
+++ src/vm/mod.rs
@@ -1,2 +1,10 @@
-use crate::ir::ir;
+use crate::ir;
 pub mod vm;
+
+// TODO: Add function definition
+// #[derive(Debug, PartialEq, Clone)]
+// pub struct StackFrame<'i> {
+//     pub pc: ProgramCounter,
+//     pub func_name: String,
+//     pub func_def: &'i ir::FunctionDefinition,
+// }
--- src/vm/vm.rs
+++ src/vm/vm.rs
@@ -1,7 +1,9 @@
 use thiserror::Error;
+use crate::ir::Instruction;
+
 use super::ir;
 
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 pub enum Value {
     Unit,
 }
@@ -12,17 +14,66 @@ pub enum VMError {
     StackUnderflow,
 }
 
-struct VM {
-    instructions: Vec<ir::Instruction>,
-    data: Vec<Value>,
+pub struct VM {
+    // instruction pointer
+    pub ip: Value,
+    pub instructions: Vec<ir::Instruction>,
+    pub data: Vec<Value>,
 }
 
 impl VM {
-    fn pop_data(&mut self) -> Result<Value, VMError> {
+    pub fn pop_data(&mut self) -> Result<Value, VMError> {
         if let Some(value) = self.data.pop() {
             Ok(value)
         } else {
             Err(VMError::StackUnderflow)
         }
     }
+
+    pub fn push_data(&mut self, value: Value) {
+        // TODO: Stack depth and stack overflow (maybe), it can be vary
+        self.data.push(value);
+    }
+
+    fn execute_nop(&mut self) -> Value {
+       Value::Unit
+    }
+
+    fn execute_instruction(&mut self, instruction: &Instruction) -> Result<ExecStep, VMError> {
+        let result = match instruction {
+            Instruction::Nop => self.execute_nop(),
+            _ => todo!()
+        };
+        Ok(ExecStep { ip: self.ip, op: instruction.clone() })
+    }
+
+    pub fn execute(&mut self) -> Result<ExecTrace, VMError> {
+        let mut log = vec![];
+        let instructions = self.instructions.clone();
+        for instr in instructions.iter() {
+            let result = self.execute_instruction(instr)?;
+            log.push(result);
+        }
+        Ok(ExecTrace { log })
+    }
+
+    pub fn evaluate(&mut self) {
+        todo!()
+    }
+}
+
+pub struct State {
+    pub ip: Value,
+
+}
+
+#[derive(Clone, Debug)]
+pub struct ExecTrace {
+    pub log: Vec<ExecStep>,
+}
+
+#[derive(Clone, Debug)]
+pub struct ExecStep {
+    pub ip: Value,
+    pub op: ir::Instruction,
 }
