59a20c2fb8f360f346efb5e3e3822ac2273619ef
jeong0982 <jeong0982@kaist.ac.kr>
2023-08-08 18:23:51 +0900

    Fix instructions to avoid using operand

--- src/ir/mod.rs
+++ src/ir/mod.rs
@@ -6,21 +6,19 @@ pub enum Instruction {
     Nop,
     BinOp {
         op: ast::BinaryOperator,
-        lhs: Operand,
-        rhs: Operand,
         // type: Type,
     },
     UnaryOp {
         op: ast::UnaryOperator,
-        operand: Operand,
         // type: Type,
-    }
+    },
+
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub enum Operand {
     Constant(Constant),
-
+    StackValue(usize),
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
--- src/vm/vm.rs
+++ src/vm/vm.rs
@@ -1,11 +1,15 @@
 use thiserror::Error;
-use crate::ir::Instruction;
+use num_bigint::BigInt;
+use crate::{ir::{Instruction, Operand}, ast::ast};
 
 use super::ir;
 
-#[derive(Debug, PartialEq, Clone, Copy)]
+#[derive(Debug, PartialEq, Clone)]
 pub enum Value {
     Unit,
+    Num {
+        value: BigInt,
+    }
 }
 
 #[derive(Debug, PartialEq, Eq, Error)]
@@ -16,13 +20,14 @@ pub enum VMError {
 
 pub struct VM {
     // instruction pointer
-    pub ip: Value,
+    pub ip: usize,
     pub instructions: Vec<ir::Instruction>,
     pub data: Vec<Value>,
+    pub address: Vec<Value>,
 }
 
 impl VM {
-    pub fn pop_data(&mut self) -> Result<Value, VMError> {
+    fn pop_data(&mut self) -> Result<Value, VMError> {
         if let Some(value) = self.data.pop() {
             Ok(value)
         } else {
@@ -30,20 +35,58 @@ impl VM {
         }
     }
 
-    pub fn push_data(&mut self, value: Value) {
+    fn push_data(&mut self, value: Value) {
         // TODO: Stack depth and stack overflow (maybe), it can be vary
         self.data.push(value);
+    }
+
+    fn pop_address(&mut self) -> Result<Value, VMError> {
+        if let Some(value) = self.address.pop() {
+            Ok(value)
+        } else {
+            Err(VMError::StackUnderflow)
+        }
+    }
+
+    fn push_address(&mut self, value: Value) {
+        self.address.push(value);
     }
 
     fn execute_nop(&mut self) -> Value {
        Value::Unit
     }
 
+    fn execute_binop(&mut self, op: &ast::BinaryOperator) -> Value {
+        let result = match op {
+            ast::BinaryOperator::Plus => {
+                Value::Unit
+            },
+            ast::BinaryOperator::Multiply => {
+                Value::Unit
+            },
+            _ => Value::Unit,
+        };
+        result
+    }
+
+    fn execute_unop(&mut self, op: &ast::UnaryOperator) -> Value {
+        let result = match op {
+            ast::UnaryOperator::Minus => {
+                Value::Unit
+            },
+            _ => Value::Unit,
+        };
+        result
+    }
+
     fn execute_instruction(&mut self, instruction: &Instruction) -> Result<ExecStep, VMError> {
         let result = match instruction {
             Instruction::Nop => self.execute_nop(),
+            Instruction::BinOp { op } => self.execute_binop(op),
+            Instruction::UnaryOp { op } => self.execute_unop(op),
             _ => todo!()
         };
+        self.ip += 1;
         Ok(ExecStep { ip: self.ip, op: instruction.clone() })
     }
 
@@ -74,6 +117,6 @@ pub struct ExecTrace {
 
 #[derive(Clone, Debug)]
 pub struct ExecStep {
-    pub ip: Value,
+    pub ip: usize,
     pub op: ir::Instruction,
 }
